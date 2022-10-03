67ef259aaf6786e716822f495d501fe89832886e
Vishal Kumar <vishalsharma.viz@gmail.com>
2022-10-03 12:18:06 +0530

    Update Exercise 1.5.c

--- Chapter 1 - A tutorial introduction/Exercise 1.5.c
+++ Chapter 1 - A tutorial introduction/Exercise 1.5.c
@@ -11,3 +11,25 @@ int main(){
         printf("%3.0f\t%6.1f\n" , fahr , celsius);
     }
 }
+
+/* It is bad to bury magic numbers like 300 , or 20 in a program.
+they convey little information to someone who might have to read the program
+later and they are hard to change in systematic way.
+
+A #define line defines a symbolic name or symbolic constant to be a particular 
+string of characters :
+
+#define    name    replacemnet text;
+*/
+
+#include <stdio.h>
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+int main(){
+    int fahr;
+    for(fahr = LOWER ; fahr <= UPPER ; fahr = fahr + STEP);
+        printf("%3d %6.1f\n" , fahr , (5.0/9.0) * (fahr-32));
+}
