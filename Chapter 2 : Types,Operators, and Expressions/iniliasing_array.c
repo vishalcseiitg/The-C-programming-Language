cb86028df9f012936e225e8f93d7584c52f1bf40
Vishal Kumar <vishalsharma.viz@gmail.com>
2022-10-09 11:49:00 +0530

    Update iniliasing_array.c

--- Chapter 2 : Types,Operators, and Expressions/iniliasing_array.c
+++ Chapter 2 : Types,Operators, and Expressions/iniliasing_array.c
@@ -15,3 +15,24 @@ int main(){
         printf("%d" , arr[i]);
     }
 }
+
+// reversing the array
+
+int main(){
+    int arr[N];
+    int n;
+    printf("Enter total elements in the array");
+    scanf("%d " , &n);
+    
+    printf("Enter elements of array");
+    
+    for(int i = 0 ; i < n ; i++){
+        scanf("%d" , &arr[i]);
+    }
+        
+        //reversed array as output
+        
+        for(int i = n-1 ; i <=0 ; i--){
+            printf("%d" , arr[i]);
+        }
+    }
